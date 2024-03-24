63cc3335fd69e383a1aac5a63b847d86d3023ff2
akaChenshunCodeMaster <91108691+akaChenshunCodeMaster@users.noreply.github.com>
2024-03-23 22:44:55 -0700

    change on the profile

--- foody/src/components/ProfilePage.js
+++ foody/src/components/ProfilePage.js
@@ -1,49 +1,29 @@
 import React from 'react';
-import styles from './ProfilePage.css'; // Make sure the path is correct
-import userAvatar from '../assets/unsplash_v2aKnjMbP_k.jpg'; 
+import './ProfilePage.css'; // Ensure path is correct
+import userAvatar from '../assets/unsplash_v2aKnjMbP_k.jpg'; // Update with the correct path
 
 const ProfilePage = () => {
-  // Hard-coded user data
-  const userData = {
-    name: 'Ryan Joseph',
-    username: '@ryan',
-    avatarSrc: userAvatar, // Path to your image file
-    postCount: '5',
-    followersCount: '123',
-    followingCount: '123',
-  };
-
-  // Hard-coded posts data - replace with actual post data
-  const userPosts = [
-    // ... Array of user post objects
-  ];
-
   return (
-    <div className={styles.profilePage}>
-      <div className={styles.profileHeader}>
-        <img src={userData.avatarSrc} alt="User Avatar" className={styles.userAvatar} />
-        <h1>{userData.name}</h1>
-        <p>{userData.username}</p>
-        <div className={styles.userStats}>
-          <span>{userData.postCount} Posts</span>
-          <span>{userData.followersCount} Followers</span>
-          <span>{userData.followingCount} Following</span>
-        </div>
-        <button className={styles.editProfileBtn}>Edit Profile</button>
-      </div>
-
-      <div className={styles.postTabs}>
-        <button className={`${styles.tabButton} ${styles.activeTabButton}`}>Posts</button>
-        <button className={styles.tabButton}>Saved</button>
-      </div>
-
-      <div className={styles.postGrid}>
-        {userPosts.map(post => (
-          <div key={post.id} className={styles.postCard}>
-            <img src={post.imageSrc} alt="Post" />
-            {/* Add other post details here */}
+    <div className="profileContainer">
+      <div className="profileCard">
+        <img src={userAvatar} alt="User Avatar" className="avatar" />
+        <h1 className="name">Ryan Joseph</h1>
+        <p className="username">@ryan</p>
+        <div className="statsContainer">
+          <div className="statItem">
+            <span className="statNumber">1</span>
+            <span className="statLabel">Post</span>
           </div>
-        ))}
+          <div className="statItem">
+            <span className="statNumber">123</span>
+            <span className="statLabel">Followers</span>
+          </div>
+          <div className="statItem">
+            <span className="statNumber">123</span>
+            <span className="statLabel">Following</span>
+          </div>
+        </div>
+        <button className="editButton">Edit Profile</button>
       </div>
     </div>
   );
